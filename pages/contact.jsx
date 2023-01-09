4bfb3af3d44c4ed5d9bd72d39bdc0b1b04d8a35c
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-01-09 21:48:53 +0530

    github push

---
+++ pages/contact.jsx
@@ -0,0 +1,21 @@
+
+const Contact = () => {
+    return (  
+        <>
+            <h2>Get In Touch!</h2>
+            <p>Thank you for visiting Cohorts with Sachin! If you have any questions or comments, we would love to hear from you.</p>
+
+            <p>You can reach us through the following channels:</p>
+
+            <ul>
+                <li>Email: mail.sachinsblog+cohort@gmail.com</li>
+            </ul>
+            <p>We strive to respond to all inquiries within one business day.</p>
+
+            <p>Thank you for choosing. I look forward to serving you!</p>
+
+        </>
+    );
+}
+ 
+export default Contact;
---
+++ pages/privacy.jsx
@@ -0,0 +1,29 @@
+
+const Privacy = () => {
+    return (  
+        <div className="col-md-8 col-11 m-auto">
+            <h2>Privacy Policy</h2>
+
+            <p>At [Company Name], we are committed to protecting the privacy of our users. This Privacy Policy explains how we collect, use, and share information about you when you use our website and online courses.</p>
+
+            <p>Information We Collect: We collect information about you when you register for one of our courses, complete a form on our website, or interact with us through other channels such as email. This information may include your name, email address, phone number, and other contact details.</p>
+
+            <p>How We Use Your Information: We use the information we collect about you to provide you with the services you request, communicate with you about your account and our courses and improve the overall user experience on our website.</p>
+
+            <p>Sharing Your Information: We will not share your personal information with any third parties without your consent unless required to do so by law.</p>
+
+            <p>Cookies: We use cookies to track your use of our website and to personalize your experience. You can choose to disable cookies in your browser settings, but this may affect the functionality of our website.</p>
+
+            <p>Data Security: We take reasonable measures to protect the security of your personal information. However, no internet transmission is completely secure, so we cannot guarantee the security of your information.</p>
+
+            <p>Changes to This Privacy Policy: We may update this Privacy Policy from time to time. Any changes will be posted on this page, and we encourage you to review our Privacy Policy regularly.</p>
+
+            <p>By using our website and enrolling in our online courses, you acknowledge that you have read and understand this Privacy Policy and agree to the collection, use, and sharing of your personal information as described. If you have any questions or concerns about our Privacy Policy, please contact us.</p>
+
+
+
+        </div>
+    );
+}
+ 
+export default Privacy;
