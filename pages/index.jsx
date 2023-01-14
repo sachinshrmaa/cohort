484d5c929b926217b551ba4b3889d878133e99a4
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-01-14 18:02:29 +0530

    added mailchimp for the waitlist

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,23 +17,36 @@ const Home = () => {
           <div className="row align-items-center">
             <div className="col-md-6 col-12">
               <h1 className={styles.heroTitle}>Cohorts</h1>
-              <p>Our workshop kit offers a range of exercises you can run with your team to put the Guidebook’s design patterns into practice in your product.</p>
+              <p>the basics offer a range of micro-courses in the form of 90-minutes live cohorts and resources you can use to put everything into practice.</p>
+   
+              <form action="https://sachinshrmaa.us10.list-manage.com/subscribe/post?u=e94ac720736227167db541b5c&amp;id=5f3f8db4af&amp;f_id=0098c6e5f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="mt-md-5 mt-4 validate" target="_self">
+                  <div id="mc_embed_signup_scroll" className="row">
 
-              <form action="https://sachinsblog.us10.list-manage.com/subscribe/post?u=5e93d1e3a83679d27d02d635f&amp;id=13e8fc99a7&amp;f_id=0050cbe5f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="mt-md-5 mt-4 validate" target="_self">
-                <input type="email"  name="EMAIL" id="mce-EMAIL" placeholder="yourmail@gmail.com" className={styles.emailInp} />
-                <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
-                
-                  <div class="response d-none" id="mce-error-response" ></div>
-                  <div class="response d-none" id="mce-success-response"></div>
-                {/* <div id="mce-responses">
-                    <div class="response d-none" id="mce-error-response" ></div>
-                    <div class="response d-none" id="mce-success-response"></div>
-                </div>  */}
-                <div className="d-none" aria-hidden="true">
-                    <input type="text" name="b_5e93d1e3a83679d27d02d635f_13e8fc99a7" tabindex="-1" value="" />
-                </div>
-                <input type="submit" value="join waitlist →" id="mc-embedded-subscribe" name="subscribe" className={styles.emailSubmit} />
-               
+                      <div className="col-md-6 col-12">
+                          <input type="email" name="EMAIL" placeholder="yourmail@gmail.com" className={styles.emailInp} id="mce-EMAIL" required />
+                          <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+
+                          <div hidden="true"><input type="hidden" name="tags" value="14152423" /></div>
+
+                          <div id="mce-responses" class="clear foot">
+                            <div class="response d-none" id="mce-error-response" ></div>
+                            <div class="response d-none" id="mce-success-response" ></div>
+                          </div>    
+
+                          {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */}
+                          <div className="form-meta" aria-hidden="true"><input type="text" name="b_e94ac720736227167db541b5c_5f3f8db4af" tabindex="-1" value="" /></div>
+
+                      </div>
+
+                      <div className="col-md-6 col-12 mt-3 mt-md-0">
+                          <div class="optionalParent">
+                            <div class="clear foot">
+                              <input type="submit" value="join waitlist →" name="subscribe" id="mc-embedded-subscribe" className={styles.emailSubmit} />
+                            </div>
+                          </div>
+                      </div>
+                    
+                  </div>
               </form>
 
               <p className="text-mute mt-2"><small>by joining you agree the <Link href='/terms' className="text-decoration-none">terms</Link> and <Link href='/privacy' className="text-decoration-none">privacy policy</Link>. </small></p>
@@ -56,12 +69,14 @@ const Home = () => {
             <div className="col-md-4 mb-3">
 
               <h2 className={styles.subHeading}>Get Started</h2>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus nihil quia a culpa!</p>
+              <p>The cohort can be accessed remotely via Google meet.</p>
               <p>To get started, you’ll need: </p>
               <ul>
-                <li>Lorem, ipsum.</li>
-                <li>Lorem, ipsum.</li>
-                <li>Lorem, ipsum.</li>
+                <li>Internet connection 🌐</li>
+                <li>Laptop/phone 💻</li>
+                <li>Valid email 📧</li>
+                <li> less than 300 Rupees 💵 (optional)</li>
+              
               </ul>
 
             </div>
@@ -76,11 +91,37 @@ const Home = () => {
                       <h2 className="card-title mt-2">Git and GitHub basics</h2>
                       <p className="card-text">Git is a free and open-source version control system, originally created by Linus Torvalds in 2005. </p>
                       <ul>
-                        <li>Live session</li>
-                        <li>QnA</li>
-                        <li>Private community</li>
+                        <li>Live session 👨🏻‍💻</li>
+                        <li>Resources 📑</li>
+                        <li>Newsletter ✉️</li>
+                        <li>Private  community 👥</li>
+                        <li>QnA 🙋</li>
                       </ul>
-                      <a href="#" className={styles.enrollBtn}>Enroll → </a>
+                      <a href="#" className={styles.enrollBtn}>Enroll Closed → </a>
+                    </div>
+                  </div>
+
+                  <div class="col-md-4 m-md-4 m-0 order-md-2 order-1 ">
+                    <img src="/img/git.png" class="img-fluid rounded-start" alt="..." />
+                  </div>
+                </div>
+              </div>
+
+              <div className="card my-5">
+                <div class="row g-0">
+                  <div class="col-md-7 order-md-1 order-2">
+                    <div className="card-body">
+                      <small className={styles.tags}>Upcomming</small>
+                      <h2 className="card-title mt-2">Introduction to Machine Learning</h2>
+                      <p className="card-text">Git is a free and open-source version control system, originally created by Linus Torvalds in 2005. </p>
+                      <ul>
+                        <li>Live session 👨🏻‍💻</li>
+                        <li>Resources 📑</li>
+                        <li>Newsletter ✉️</li>
+                        <li>Private  community 👥</li>
+                        <li>QnA 🙋</li>
+                      </ul>
+                      <a href="#" className={styles.enrollBtn}>Enroll Closed  ₹ 249.0 → </a>
                     </div>
                   </div>
 
