e0dc691129d1582fafc4a80dd97aaad0cca0f863
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-07-22 13:48:59 +0530

    fix: add fav icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </head>
       <body
         className={`${poppins.variable} ${albert_sans.variable} font-poppins font-albert`}
       >
